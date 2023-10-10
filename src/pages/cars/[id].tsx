91eb8c7f49676b9278e6588c3e34a2bb5f29f5dd
kevinmundia6771 <kevinmaina6771@gmail.com>
2023-10-10 16:03:43 +0300

    refactor: :lipstick: car details Grid

--- src/pages/cars/[id].tsx
+++ src/pages/cars/[id].tsx
@@ -1,47 +1,59 @@
-import CarouselThumbs from '@components/CarouselThumbs';
-import MainSwiperImage from '@components/MainSwiperImage';
-import RootLayout from '@layouts/RootLayout';
-import { Container, Stack } from '@mui/material';
-import { baseUrl } from '@utils/constants';
-import { CarMedia } from '@utils/types';
-import { GetServerSideProps } from 'next';
-import { useState } from 'react';
+import CarouselThumbs from "@components/CarouselThumbs";
+import MainSwiperImage from "@components/MainSwiperImage";
+import RootLayout from "@layouts/RootLayout";
+import { Container, Grid, Stack } from "@mui/material";
+import { baseUrl } from "@utils/constants";
+import { Car, CarMedia } from "@utils/types";
+import { GetServerSideProps } from "next";
+import { useState } from "react";
 
 interface ICarProps {
-	media: CarMedia;
+  media: CarMedia;
+  car: Car;
 }
 
-export default function CarDetails({ media }: ICarProps) {
-	const [thumbsSwiper, setThumbsSwiper] = useState(null);
-	return (
-		<RootLayout>
-			<Container sx={{ py: 8, mx: '0 auto' }}>
-				<Stack sx={{ maxWidth: { xs: '100%', md: 'sm' } }}>
-					<MainSwiperImage
-						media={media.carMediaList}
-						thumbsSwiper={thumbsSwiper}
-					/>
-					<Stack sx={{ display: { xs: 'none', md: 'flex' } }}>
-						<CarouselThumbs
-							media={media.carMediaList}
-							setThumbSwiper={setThumbsSwiper}
-						/>
-					</Stack>
-				</Stack>
-			</Container>
-		</RootLayout>
-	);
+export default function CarDetails({ media, car }: ICarProps) {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  return (
+    <RootLayout>
+      <Container sx={{ py: 8, mx: "0 auto" }}>
+        <Grid container spacing={2}>
+          <Grid xs={6} marginLeft={2}>
+            <Stack sx={{ maxWidth: { xs: "100%", md: "sm" } }}>
+              <MainSwiperImage
+                media={media.carMediaList}
+                thumbsSwiper={thumbsSwiper}
+              />
+              <Stack sx={{ display: { xs: "none", md: "flex" } }}>
+                <CarouselThumbs
+                  media={media.carMediaList}
+                  setThumbSwiper={setThumbsSwiper}
+                />
+              </Stack>
+            </Stack>
+          </Grid>
+          <Grid xs={6} marginLeft={2}>
+            <Grid xs={6}></Grid>
+            <Grid xs={6}></Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </RootLayout>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-	// const resp = await fetch(`${baseUrl}/car/${params.id}`);
+  const resp = await fetch(`${baseUrl}/car/${params?.id}`);
+  console.log(await resp.json());
+  const car = await resp.json();
 
-	const mediaResp = await fetch(`${baseUrl}/car_media?carId=${params?.id}`);
-	const media: CarMedia = await mediaResp.json();
+  const mediaResp = await fetch(`${baseUrl}/car_media?carId=${params?.id}`);
+  const media: CarMedia = await mediaResp.json();
 
-	return {
-		props: {
-			media,
-		},
-	};
+  return {
+    props: {
+      media,
+      car,
+    },
+  };
 };
